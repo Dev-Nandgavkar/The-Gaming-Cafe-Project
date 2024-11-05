1b9b595207ae7bcdf022663fe1c064dee33a57e8
Dev-Nandgavkar <devnandgavkar@gmail.com>
2024-11-06 02:57:50 +0530

    up 06-11-2024 02:57

--- JS/script.js
+++ JS/script.js
@@ -9,4 +9,51 @@ window.addEventListener('scroll', ()=>{
 })
 
 
+// Customer Reviews Slider 
+
+$('.comentslick').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    adaptiveHeight: true,
+    responsive: [
+        {
+            breakpoint: 1400,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                // infinite: true,
+                // dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                // infinite: true,
+                // dots: true
+            }
+        }
+    ]
+})
 
